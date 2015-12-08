df59cadfe0034392c205e07d52163e6c8e218431
Ricky Hougland <houglande@gmail.com>
2015-12-08 11:35:48 -0800

    add relationships between models

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,9 @@
 class MerchantsController < ApplicationController
+  def new
+    # display the form to sign up as a merchant
+  end
+
+  def create
+    # create the new merchant then display merchant home page
+  end
 end
