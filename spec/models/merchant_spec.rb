481bda5ee0d6fc9f4e54c9226b120808a5843031
Ricky Hougland <houglande@gmail.com>
2015-12-08 14:53:15 -0800

    dry up tests for merchant using 'let' + add tests for presense of order and product to orderitem model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,33 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  let(:empty_merchant) do
+      Merchant.new()
+  end
+  let(:good_merchant) do
+    Merchant.create({
+      username: "merchant",
+      email: "sdfs@sdf.com",
+      password: "123",
+      password_confirmation: "123"
+    })
+  end
+
   describe "model validations" do
       it "must have a username" do
-        test_merchant = Merchant.new()
-        expect(test_merchant).to_not be_valid
-        expect(test_merchant.errors.keys).to include :username
+        expect(empty_merchant).to_not be_valid
+        expect(empty_merchant.errors.keys).to include :username
       end
       it "must have an email" do
-        test_merchant = Merchant.new()
-        expect(test_merchant).to_not be_valid
-        expect(test_merchant.errors.keys).to include :email
+        expect(empty_merchant).to_not be_valid
+        expect(empty_merchant.errors.keys).to include :email
       end
       it "must have a unique username" do
-        Merchant.create({ username: "merchant", email: "sdfs@sdf.com", password: "123", password_confirmation: "123" })
-        test_merchant2 = Merchant.new({ username: "merchant", email: "gdfs@sdf.com", password: "123", password_confirmation: "123" })
-        expect(test_merchant2.save).to eq false
-        expect(test_merchant2.errors.keys).to include :username
+        good_merchant
+        non_unique_username = Merchant.new({ username: "merchant", email: "gdfs@sdf.com", password: "123", password_confirmation: "123" })
+        expect(non_unique_username.save).to eq false
+        expect(non_unique_username.errors.keys).to include :username
       end
       it "must have a unique email" do
-        Merchant.create({ username: "merchant1", email: "sdfs@sdf.com", password: "123", password_confirmation: "123" })
-        test_merchant2 = Merchant.new({ username: "merchant2", email: "sdfs@sdf.com", password: "123", password_confirmation: "123" })
-        expect(test_merchant2.save).to eq false
-        expect(test_merchant2.errors.keys).to include :email
+        good_merchant
+        non_unique_email = Merchant.new({ username: "merchant2", email: "sdfs@sdf.com", password: "123", password_confirmation: "123" })
+        expect(non_unique_email.save).to eq false
+        expect(non_unique_email.errors.keys).to include :email
       end
       it "must have a password confirmation that matches password" do
-        test_merchant = Merchant.new({ username: "merchant1", email: "sdfs@sdf.com", password: "123", password_confirmation: "321" })
-        expect(test_merchant.save).to eq false
-        expect(test_merchant.errors.keys).to include :password_confirmation
+        passwords_not_matching = Merchant.new({ username: "merchant1", email: "sdfs@sdf.com", password: "123", password_confirmation: "321" })
+        expect(passwords_not_matching.save).to eq false
+        expect(passwords_not_matching.errors.keys).to include :password_confirmation
       end
     end
 end
