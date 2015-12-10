709ecd0c75c8e8efa14be7816ccf1b119bfefce4
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-10 15:45:44 -0800

    added orders/checkout page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
-  def new
+  def checkout
     # get @current_order info from carts controller
+    @cart_items = session[:cart]
+    @order = Order.new
+  end
+
+  def confirm
+    @order = Order.create(order_params)
+    @order_item = OrderItem.create()
+    if @order.save
+      redirect_to @order
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(order:[:purchase_time, :name, :email, :street, :city, :state, :zip, :cc_num, :cc_exp, :sec_code, :bill_zip])
   end
 end
