7fdec7fc619d089f437cdade98f4202331e8b9dd
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-11 10:02:18 -0800

    order checkout spec now works

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
   let(:cart_items) do
-    { "1" => 2 }
+    { product.id => 2 }
   end
+
+  let(:product) do
+    Product.create(name: "Stuff", price: 500)
+  end
+
+  before :each do
+    session[:cart] = cart_items
+  end
+
 
   describe "GET 'checkout'" do
     it "renders the checkout page" do
-      get :checkout, cart_items
+      get :checkout
       expect(subject).to render_template :checkout
     end
   end
