c542736071c9982fb2ddfcc4b3ebf2f570898c84
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-12 19:24:43 -0800

    applied navbar partials to all views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :merchants_for_navbar
+
   def current_merchant
     @current_merchant ||= Merchant.find(session[:user_id]) if session[:user_id]
   end
@@ -15,6 +17,10 @@ class ApplicationController < ActionController::Base
       flash[:error] = "Please log in to view this section."
       redirect_to new_session_path
     end
+  end
+
+  def merchants_for_navbar
+    @merchants = Merchant.all
   end
 
 end
