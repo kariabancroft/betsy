46b04bed7fb9368d3a8f342c6dc2b050c682eaf7
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-12 19:43:13 -0800

    fixed navbar links and added link to merchant dashboard when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+# this before action retrieves all the merchants for the navbar links on every view
   before_action :merchants_for_navbar
+# this before action checks to see if there is a current merchant and allows the merchant navbar to always link to the merchant's home page (dashboard)
+  before_action :current_merchant
 
   def current_merchant
     @current_merchant ||= Merchant.find(session[:user_id]) if session[:user_id]
