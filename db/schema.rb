1f5fe7d8039bd607b48e944897d7fa1705bb19d3
Sarah <trowbrsa@gmail.com>
2015-12-14 11:37:12 -0800

    Undid the migration to remove image columns on merchants

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214190132) do
-
+ActiveRecord::Schema.define(version: 20151214185550) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,8 +36,12 @@ ActiveRecord::Schema.define(version: 20151214190132) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.datetime "image_updated_at"
+    t.integer  "image_file_size"
+    t.string   "image_content_type"
+    t.string   "image_file_name"
   end
 
   create_table "order_items", force: :cascade do |t|
