7f986396456beace26b8eff8096a8330dce91129
Ricky Hougland <houglande@gmail.com>
2015-12-16 10:12:31 -0800

    add has many through relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :order_items
+  has_many :products, through: :order_items
 
   validates_presence_of :name, :bill_zip, :cc_exp, :cc_num, :city, :email, :sec_code, :state, :street, :zip
   validates :city, length: { maximum: 30 }
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :merchant
   has_many :reviews
   has_many :order_items
+  has_many :orders, through: :order_items
   has_and_belongs_to_many :categories
 
   validates_associated :reviews
