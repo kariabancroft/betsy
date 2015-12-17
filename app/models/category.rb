d689297664e873ee52d3af576f77d0937c948c2b
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-17 14:06:58 -0800

    changed size of home page images for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
     has_and_belongs_to_many :products
     validates :name, presence: true, uniqueness: true
-    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>", home: "530x300" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>", home: "480x300" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
