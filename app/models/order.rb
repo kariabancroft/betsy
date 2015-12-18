a06bb1fed852a57e1ee535f2241d2496be01fa82
Desiree <desireepoland@gmail.com>
2015-12-18 13:20:14 -0800

    comment out length validation for now

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
 
   validates_presence_of :name, :bill_zip, :cc_exp, :cc_num, :city, :email, :sec_code, :state, :street, :zip
   validates :city, length: { maximum: 30 }
-  validates :cc_num, length: { is: 4 }
+  # validates :cc_num, length: { is: 4 }
   validates :status, inclusion: { in: %w(Pending Paid Complete Cancelled) }
 
   def total_cost
