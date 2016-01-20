f6a222e82e96c1648839dc127da0ea79184919cc
Wesley Willis <haileyw@gmail.com>
2016-01-19 16:37:36 -0800

    add seed data to match new migration fields in product and merchant

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119232322) do
+ActiveRecord::Schema.define(version: 20160120002834) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 20160119232322) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "country"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
   end
 
   create_table "order_items", force: :cascade do |t|
