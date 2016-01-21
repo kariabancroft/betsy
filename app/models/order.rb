99a2ad5ab28d3197cd90bbecc77bc65cd6f45ea0
Logan McDonald <loganmcdona11@gmail.com>
2016-01-21 14:20:43 -0800

    updated the product and the confimation page for us

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,6 +14,7 @@ class Order < ActiveRecord::Base
 
     order_items.each do |orderitem|
       total_revenue += orderitem.cost
+      total_revenue += orderitem.shipping_cost
     end
 
     return total_revenue
